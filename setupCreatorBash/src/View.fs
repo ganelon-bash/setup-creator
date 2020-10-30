184a52dcc9d7853822c627098f184c7bd070194a
VladP00 <g.v.paun@student.rug.nl>
2020-10-30 06:43:19 +0100

    removed extra files

--- setupCreatorBash/src/View.fs
+++
@@ -1,6 +0,0 @@
-module App.View
-open App.RightSideBar
-open App.Model
-
-let view model dispatch =
-        RightSideBar.rightSideBar model.title model.author model.dayLength model.nightLength model.lynchType dispatch
